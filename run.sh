c94b20c4c46cae3bd8a773dfd5f8d5e6325e5a87
minovermax <>
2022-07-03 13:57:09 +0000

    07/03/22 starting data structures with C

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-gcc *.c -o tictactoe
-chmod 744 tictactoe
-./tictactoe
+export WORKING_ON="tictactoe"
+
+gcc *.c -o $WORKING_ON
+chmod 744 $WORKING_ON
+./$WORKING_ON
+
+# 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
